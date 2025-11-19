90fcd0a145162dcce390726e931e492509868614
ophiase <ophiase@outlook.com>
2025-11-19 12:25:19 +0100

    wip kafka integration

--- mastodon-fetcher/sender.py
+++ mastodon-fetcher/sender.py
@@ -1,12 +1,24 @@
+from kafka import KafkaProducer
 from typing import Any, Dict, List
+from constants import KAFKA_SERVER, KAFKA_PORT, KAFKA_RAW_TOPIC
+import json
 
 
 class Sender:
-    def __init__(self):
-        pass
+    def __init__(self, bootstrap_servers: str = f"{KAFKA_SERVER}:{KAFKA_PORT}") -> None:
+        self.producer = KafkaProducer(
+            bootstrap_servers=bootstrap_servers,
+            value_serializer=lambda v: json.dumps(v).encode('utf-8')
+        )
 
-    def __call__(self, posts: List[Dict[str, Any]], *args, **kwds):
-        self.send_kafka(posts)
+    def __call__(self, posts: List[Dict[str, Any]]) -> None:
+        self.send_posts(posts)
 
-    def send_kafka(self, posts: List[Dict[str, Any]]) -> None:
+    def send_posts(self, posts: List[Dict[str, Any]]) -> None:
         print(f"Sending {len(posts)} posts to Kafka...")
+        for post in posts:
+            self.send(KAFKA_RAW_TOPIC, post)
+
+    def send(self, topic: str, message: Dict[str, Any]) -> None:
+        self.producer.send(topic, message)
+        self.producer.flush()
