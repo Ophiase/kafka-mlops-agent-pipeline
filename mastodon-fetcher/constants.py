db603122da057507447ef1edb8271c1f51fbcea6
ophiase <ophiase@outlook.com>
2025-11-20 23:12:19 +0100

    minor

--- mastodon-fetcher/constants.py
+++ mastodon-fetcher/constants.py
@@ -1,8 +1,8 @@
-import os
+from os import environ
 
-FETCH_INTERVAL_MS = int(os.environ.get("FETCH_INTERVAL_MS", 10 * 1000))
-FETCH_NUMBER = int(os.environ.get("FETCH_NUMBER", 20))
+FETCH_INTERVAL_MS = int(environ.get("FETCH_INTERVAL_MS", 10 * 1000))
+FETCH_NUMBER = int(environ.get("FETCH_NUMBER", 20))
 
-KAFKA_SERVER = os.environ.get("KAFKA_SERVER", "kafka")
-KAFKA_PORT = int(os.environ.get("KAFKA_PORT", 9092))
-KAFKA_RAW_TOPIC = os.environ.get("KAFKA_RAW_TOPIC", "mastodon_raw_posts")
+KAFKA_SERVER = environ.get("KAFKA_SERVER", "kafka")
+KAFKA_PORT = int(environ.get("KAFKA_PORT", 9092))
+KAFKA_RAW_TOPIC = environ.get("KAFKA_RAW_TOPIC", "mastodon_raw_posts")
