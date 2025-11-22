147cb8fea28268dc417deaf64aff577fe30db27d
ophiase <ophiase@outlook.com>
2025-11-22 16:49:36 +0100

    fixed ollama

--- llm-server/start_ollama.sh
+++ llm-server/start_ollama.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+
 ollama serve &
-ollama pull qwen:0.6b
+until curl -sf http://localhost:11434/api/tags >/dev/null; do
+    sleep 1
+done
+
+ollama pull qwen3:0.6b
 wait
--- metrics-processor/constants.py
+++ metrics-processor/constants.py
@@ -1,7 +1,7 @@
 from os import environ
 
 
-OLLAMA_SERVER_URL = environ.get("OLLAMA_SERVER_URL", "localhost")
+OLLAMA_SERVER_URL = environ.get("OLLAMA_SERVER_URL", "llm-server")
 OLLAMA_SERVER_PORT = environ.get("OLLAMA_SERVER_PORT", "11434")
 OLLAMA_MODEL = environ.get("OLLAMA_MODEL", "qwen3:0.6b")
 
