0fd15aa79328c4ca8180162a486f3c82ec6a2e53
ophiase <ophiase@outlook.com>
2025-11-22 11:40:01 +0100

    wip libs

--- metrics-processor/processor.py
+++ metrics-processor/processor.py
@@ -1,4 +1,4 @@
-from langchain_community.llms import Ollama
+from langchain_ollama.llms import OllamaLLM
 from langchain.chains import ConversationChain
 from langchain.memory import ConversationBufferMemory
 from constants import OLLAMA_SERVER_URL, OLLAMA_SERVER_PORT, OLLAMA_MODEL
@@ -10,11 +10,11 @@ class Processor:
         llm = self.build_llm(model)
         self.chain = self.build_chain(llm)
 
-    def build_llm(self, model: str) -> Ollama:
+    def build_llm(self, model: str) -> OllamaLLM:
         ollama_server_url = f"{OLLAMA_SERVER_URL}:{OLLAMA_SERVER_PORT}"
-        return Ollama(model=model, base_url=ollama_server_url)
+        return OllamaLLM(model=model, base_url=ollama_server_url)
 
-    def build_chain(self, llm: Ollama) -> ConversationChain:
+    def build_chain(self, llm: OllamaLLM) -> ConversationChain:
         memory = ConversationBufferMemory()
         return ConversationChain(llm=llm, memory=memory)
 
