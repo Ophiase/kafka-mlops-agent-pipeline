c90e02a1ffb697add5a0e07cc956d8572c17e91c
ophiase <ophiase@outlook.com>
2025-11-22 16:33:18 +0100

    fix processor

--- metrics-processor/processor.py
+++ metrics-processor/processor.py
@@ -1,22 +1,38 @@
-from langchain_ollama.llms import OllamaLLM
-from langchain.chains import ConversationChain
-from langchain.memory import ConversationBufferMemory
-from constants import OLLAMA_SERVER_URL, OLLAMA_SERVER_PORT, OLLAMA_MODEL
+import json
+from typing import List, Dict, Any
+from constants import OLLAMA_MODEL, OLLAMA_SERVER_URL, OLLAMA_SERVER_PORT
+from langchain_ollama import ChatOllama
+from langchain.messages import SystemMessage, HumanMessage
+from pathlib import Path
 
 
 class Processor:
-    def __init__(self):
-        model = OLLAMA_MODEL
-        llm = self.build_llm(model)
-        self.chain = self.build_chain(llm)
+    prompt: SystemMessage
+    llm: ChatOllama
 
-    def build_llm(self, model: str) -> OllamaLLM:
-        ollama_server_url = f"{OLLAMA_SERVER_URL}:{OLLAMA_SERVER_PORT}"
-        return OllamaLLM(model=model, base_url=ollama_server_url)
+    def __init__(self,
+                 prompt_path: str = "prompts/sentiment_analysis.txt",
+                 model: str = OLLAMA_MODEL,
+                 base_url: str = f"http://{OLLAMA_SERVER_URL}:{OLLAMA_SERVER_PORT}"):
+        self.prompt = self.load_prompt(prompt_path)
+        self.llm = self.build_llm(model, base_url)
 
-    def build_chain(self, llm: OllamaLLM) -> ConversationChain:
-        memory = ConversationBufferMemory()
-        return ConversationChain(llm=llm, memory=memory)
+    def __call__(self, posts: List[str]) -> List[Dict[str, Any]]:
+        msgs = self.build_messages(posts)
+        ai_msg = self.llm.invoke(msgs)
+        return ai_msg.content
+        # return self.parse(ai_msg.content)
 
-    def __call__(self, prompt: str, *args, **kwds):
-        return self.chain.invoke(prompt)
+    def load_prompt(self, path: str) -> SystemMessage:
+        return SystemMessage(Path(path).read_text().strip())
+
+    def build_llm(self, model: str, base_url: str) -> ChatOllama:
+        return ChatOllama(model=model, base_url=base_url)
+
+    def build_messages(self, posts: List[str]):
+        system = self.prompt
+        humans = [HumanMessage(post) for post in posts]
+        return [system] + humans
+
+    def parse(self, content: str) -> List[Dict[str, Any]]:
+        return json.loads(content)
--- metrics-processor/server.py
+++ metrics-processor/server.py
@@ -1,30 +1,43 @@
-# from processor import Processor
+from processor import Processor
 from kafka import KafkaConsumer
 from constants import KAFKA_PORT, KAFKA_PROCESSED_TOPIC, KAFKA_RAW_TOPIC, KAFKA_SERVER
+from constants import OLLAMA_MODEL, OLLAMA_SERVER_URL, OLLAMA_SERVER_PORT
 
 
 class Server:
-    consumer: KafkaConsumer
-    # processor: Processor
+    # consumer: KafkaConsumer
+    processor: Processor
 
     def __init__(self):
-        print(
-            f"Arguments: {KAFKA_SERVER}:{KAFKA_PORT}, topic={KAFKA_RAW_TOPIC}")
+        # self.consumer = KafkaConsumer(
+        #     KAFKA_RAW_TOPIC,
+        #     bootstrap_servers=f"{KAFKA_SERVER}:{KAFKA_PORT}",
+        #     auto_offset_reset='earliest')
 
-        self.consumer = KafkaConsumer(
-            KAFKA_RAW_TOPIC,
-            bootstrap_servers=f"{KAFKA_SERVER}:{KAFKA_PORT}",
-            auto_offset_reset='earliest')
+        self.processor = Processor(
+            model=OLLAMA_MODEL,
+            base_url=f"http://{OLLAMA_SERVER_URL}:{OLLAMA_SERVER_PORT}"
+        )
 
-        # self.processor = Processor()
+        prompts = [
+            "I love the new design of your website!",
+            "The product quality has significantly improved over the years.",
+            "Customer service was unhelpful and rude.",
+            "I'm extremely satisfied with my purchase experience.",
+            "The delivery was delayed and the package arrived damaged."
+        ]
+
+        result = self.processor(prompts)
+        print(result)
 
     def run(self):
         print("Metrics Processor Server is Listening...")
 
         # test_string = "Hello, world! who are you?"
-        # response = self.processor(test_string)
+        # response = self.processor([test_string])
         # print("Response:", response)
 
-        for message in self.consumer:
-            print("Received message:", message.value.decode('utf-8'))
-            # processed = self.processor(message.value.decode('utf-8'))
+        # for message in self.consumer:
+        #     print("Received message:", message.value.decode('utf-8'))
+        #     processed = self.processor([message.value.decode('utf-8')])
+        #     print("Processed:", processed)
