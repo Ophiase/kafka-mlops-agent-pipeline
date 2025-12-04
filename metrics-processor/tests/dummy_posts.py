fb2352c4160050117b18a0403a89cb2f22389b0b
ophiase <ophiase@outlook.com>
2025-12-04 14:42:08 +0100

    we need to easily be able to debug langgraph

---
+++ metrics-processor/tests/dummy_posts.py
@@ -0,0 +1,50 @@
+POST_00 = {
+    "id": 1,
+    "created": "2024-01-01T12:00:00Z",
+    "author": "user1",
+    "text": "Happy New Year!",
+    "tags": ["newyear", "celebration"],
+    "url": "https://mastodon.social/@user1/1",
+}
+POST_LABEL_00 = "positive"
+
+POST_01 = {
+    "id": 2,
+    "created": "2024-01-02T15:30:00Z",
+    "author": "user2",
+    "text": "Just had a great lunch.",
+    "tags": ["food", "lunch"],
+    "url": "https://mastodon.social/@user2/2",
+}
+POST_LABEL_01 = "positive"
+
+POST_02 = {
+    "id": 3,
+    "created": "2024-01-03T09:45:00Z",
+    "author": "user3",
+    "text": "Oh no, it's Monday again.",
+    "tags": ["morning", "greetings"],
+    "url": "https://mastodon.social/@user3/3",
+}
+POST_LABEL_02 = "negative"
+
+POST_03 = {
+    "id": 4,
+    "created": "2024-01-04T20:15:00Z",
+    "author": "user4",
+    "text": "Looking forward to the weekend! Maybe it will be the worst day of my life :(. Or maybe not!",
+    "tags": ["weekend", "plans"],
+    "url": "https://mastodon.social/@user4/4",
+}
+POST_LABEL_03 = "mixed"
+
+__all__ = [
+    "POST_00",
+    "POST_LABEL_00",
+    "POST_01",
+    "POST_LABEL_01",
+    "POST_02",
+    "POST_LABEL_02",
+    "POST_03",
+    "POST_LABEL_03",
+]
---
+++ metrics-processor/tests/test_inference.py
@@ -0,0 +1,10 @@
+from .dummy_posts import *
+
+
+def build_posts_xy():
+    return [
+        (POST_00, POST_LABEL_00),
+        (POST_01, POST_LABEL_01),
+        (POST_02, POST_LABEL_02),
+        (POST_03, POST_LABEL_03),
+    ]
