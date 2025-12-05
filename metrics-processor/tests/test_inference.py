70f29ce93bee6d5b20b41e5e09b2aeaaed9971e7
ophiase <ophiase@outlook.com>
2025-12-05 15:51:13 +0100

    wip test

--- metrics-processor/tests/test_inference.py
+++ metrics-processor/tests/test_inference.py
@@ -1,4 +1,7 @@
+from pprint import pprint
+from typing import Dict, Any, List
 from .dummy_posts import *
+from metrics_processor.processor import Processor
 
 
 def build_posts_xy():
@@ -10,16 +13,42 @@ def build_posts_xy():
     ]
 
 
-def verify_post(post: dict, expected_label: str):
-    label = ""
+def build_processor() -> Processor:
+    print("Building Processor...")
+    return Processor()
+
+
+def verify_output(
+        output: Dict[str, Any],
+        expected_label: str) -> bool:
+    return output["sentiment"] == expected_label
+
+
+def verify_batch(
+        processor: Processor,
+        posts: List[Dict[str, Any]],
+        expected_labels: List[Dict[str, str]]
+) -> List[bool]:
+    print("Verifying batch...")
+    processed_outputs = processor(posts)
+    print("Processed Outputs:")
+    zipped = zip(processed_outputs, expected_labels)
+    results = [
+        verify_output(output, expected_label)
+        for output, expected_label in zipped
+    ]
+    return results
 
 
 def main():
     posts_xy = build_posts_xy()
-    for post, label in posts_xy:
-        print("POST:", post)
-        print("LABEL:", label)
-        print()
+    posts_x, posts_y = zip(*posts_xy)
+    processor = build_processor()
+    results = verify_batch(
+        processor, posts_x, posts_y
+    )
+
+    pprint(results)
 
 
 if __name__ == "__main__":
